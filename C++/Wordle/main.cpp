ed91a0e417133dc1877922fa7cb3ab07222530aa
Eli Paulsen <eli-paulsen@uiowa.edu>
2023-07-04 14:50:03 -0500

    final clean

--- C++/Wordle/main.cpp
+++ C++/Wordle/main.cpp
@@ -13,43 +13,43 @@
 using namespace std;
 
 int main() {
-    Wordle game("wordList.txt");
-    game.loadWordList();          //loads in wordList.txt
-    game.setRandomSecretWord();      // sets random word
+  Wordle game("wordList.txt");
+  game.loadWordList();          //loads in wordList.txt
+  game.setRandomSecretWord();      // sets random word
 
 
-    //game intro / Instructions
-    cout << "Welcome to Wordle" << "\nYou have six tries to guess a five-letter english word." << endl
-         << "press enter to submit your guess" << endl
-         << "After each guess, you will get feedback to how close your guess was to the word. \n"
-            "! is the correct letter in the correct spot. \n"
-            "+ is the correct letter in the wrong spot. \n"
-            ". is a letter that does not appear in the word. ";
+  //game intro / Instructions
+  cout << "Welcome to Wordle" << "\nYou have six tries to guess a five-letter english word." << endl
+     << "press enter to submit your guess" << endl
+     << "After each guess, you will get feedback to how close your guess was to the word. \n"
+      "! is the correct letter in the correct spot. \n"
+      "+ is the correct letter in the wrong spot. \n"
+      ". is a letter that does not appear in the word. ";
 
-    int i = 0;
-    string tmpGuess;
-    bool win = false;
-    while(i < 6){
-        cout << "enter guess: " << endl;
-        cin >> tmpGuess;    //reads in users guess and stores it in temporary variable
-        if(game.makeGuess(tmpGuess)){     //if guess is correct then end the loop and set win as true
-            i = 6;
-            win = true;
-            break;
-        }
-        i = game.getGuessNum();   //set i = to the number of valid guesses user has made
-        if(i != 6){
-            game.print();         //prints feedback and guesses after each guess
-            game.printAvailableLetters();       //prints available letters after every guess
-        }
+  int i = 0;
+  string tmpGuess;
+  bool win = false;
+  while(i < 6){
+    cout << "enter guess: " << endl;
+    cin >> tmpGuess;    //reads in users guess and stores it in temporary variable
+    if(game.makeGuess(tmpGuess)){     //if guess is correct then end the loop and set win as true
+      i = 6;
+      win = true;
+      break;
     }
-    if(win){    //if user guesses word successfully
-        game.print();
-        cout << "WINNER: You guessed this wordle in " << game.getGuessNum() << " attempt(s)!" << endl;
+    i = game.getGuessNum();   //set i = to the number of valid guesses user has made
+    if(i != 6){
+      game.print();         //prints feedback and guesses after each guess
+      game.printAvailableLetters();       //prints available letters after every guess
     }
-    else{       //if user fails to solve wordle
-        game.print();
-        cout << "You could not complete this wordle, better luck next time" << endl;
-    }
+  }
+  if(win){    //if user guesses word successfully
+    game.print();
+    cout << "WINNER: You guessed this wordle in " << game.getGuessNum() << " attempt(s)!" << endl;
+  }
+  else{       //if user fails to solve wordle
+    game.print();
+    cout << "You could not complete this wordle, better luck next time" << endl;
+  }
 
 }
